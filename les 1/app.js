55651d26dd2529ef58d39a1503a040c9137ab113
mert <35262@ma-web.nl>
2022-11-22 12:18:19 +0100

    opdrachten van les 1 uitgevoerd

---
+++ les 1/app.js
@@ -0,0 +1,20 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        let appnaam = "valorant"
+        let versienummer = 1.1
+        let versiedatum = 11/22/2022
+        let autheur = "mert"
+        let copyright = "no"
+        let distributeur = "mert" 
+        let darkmode = true
+        console.log(appnaam, versienummer, versiedatum, autheur, copyright, distributeur, darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+
