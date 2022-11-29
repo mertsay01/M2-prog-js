0d2e944db584e0ff879969f988b2267645a04361
mert <35262@ma-web.nl>
2022-11-29 13:08:46 +0100

    les 2 correct uitgevoerd + begonnen aan les 3

--- les 2/canvashuis/app.js
+++ les 2/canvashuis/app.js
@@ -2,64 +2,73 @@ class App
 {
     runApplication()
     {
-        console.log("hello world");
         let canvas = document.getElementById("canvasId")
         let g = canvas.getContext("2d")
         g.fillRect(0,0,10,10);
+        console.log(canvas)
+
         //dak
-        g.beginPath();
+        g.beginPath()
         g.fillStyle = "yellow";
-        g.moveTo(100, 260);
-        g.lineTo(300,25);
-        g.lineTo(500, 260);
+        g.moveTo(300, 100);
+        g.lineTo(200, 300);
+        g.lineTo(600, 400);
+        g.lineTo(700, 200);
         g.closePath();
         g.stroke();
-        g.fill();
-        //huis
-        g.beginPath();
-        g.moveTo(100, 260);
-        g.lineTo(100, 500);
+        g.fill()
+        //dak 2
+        g.beginPath()
+        g.fillStyle = "black";
+        g.moveTo(700, 200);
+        g.lineTo(600, 400);
+        g.lineTo(800, 300);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        //huis zijkant groot
+        g.beginPath()
+        g.fillStyle = "grey";
+        g.moveTo(200, 300);
+        g.lineTo(200, 500);
+        g.lineTo(600, 600);
+        g.lineTo(600, 400);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        //zijkant huis klein
+        g.beginPath()
+        g.fillStyle = "blue";
+        g.moveTo(600, 400);
+        g.lineTo(600, 600);
+        g.lineTo(800, 500);
+        g.lineTo(800, 300);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        //raam 1
+        g.beginPath()
+        g.fillStyle = "green";
+        g.moveTo(300, 400);
+        g.lineTo(300, 450);
+        g.lineTo(350, 450);
+        g.lineTo(350, 400);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        //raam 2
+        g.beginPath()
+        g.fillStyle = "red";
         g.moveTo(500, 500);
-        g.lineTo(100, 500);
-        g.moveTo(100, 260);
-        g.lineTo(100, 260);
-        g.lineTo(500, 260);
-        g.moveTo(500,260);
-        g.lineTo(500,500);
+        g.lineTo(500, 450);
+        g.lineTo(450, 450);
+        g.lineTo(450, 500);
         g.closePath();
         g.stroke();
-        //raam
-        g.beginPath();
-        g.moveTo(450, 300);
-        g.lineTo(450, 400);
-        g.moveTo(450, 300);
-        g.lineTo(350, 300);
-        g.moveTo(350, 300);
-        g.lineTo(350, 400);
-        g.moveTo(450, 400);
-        g.lineTo(350, 400);
-        g.moveTo(350, 350);
-        g.lineTo(450, 350);
-        g.moveTo(400, 400);
-        g.lineTo(400, 300);
-        g.closePath();
-        g.stroke();
-        //deur
-        g.beginPath();
-        g.moveTo(150, 500);
-        g.lineTo(150, 400);
-        g.moveTo(200, 500);
-        g.lineTo(200, 400);
-        g.moveTo(150, 400);
-        g.lineTo(200, 400);
-        g.closePath();
-        g.stroke();
-        
+        g.fill()
+
+   
        
-
-        
-        
-
     }
 }
 
---
+++ les 3/app.js
@@ -0,0 +1,16 @@
+//dit is commentaar voor javascript
+// ik denk dat je de tag kan selecteren met let id en let p
+
+let title = document.getElementById("newstitle")
+let randomGetal = Math.random();
+if (randomGetal == 0) {
+    
+    title.style.backgroundColor = "#00FF00";
+}
+
+else if (randomGetal < 0.2) {
+
+    title.style.backgroundColor = "#000000";
+}
+
+else if (randomGetal )
